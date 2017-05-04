727b53443a1a796cc18a183f5ab56fc3940f8085
Apoorva Dave <Apoorva Dave>
2017-05-04 10:15:13 +0530

    Save model and plot ROC curve.

--- amazon-food-reviews-multinomial.py
+++ amazon-food-reviews-multinomial.py
@@ -1,28 +1,33 @@
 import string
-
 import pandas as pd
 from decorator import getfullargspec
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 from nltk.sentiment.util import mark_negation
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB, BernoulliNB
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 from nltk.stem import PorterStemmer
 import nltk
 from sklearn.base import TransformerMixin
+from sklearn.linear_model import LogisticRegression
 import random
 import re
-
+import numpy as np
+import matplotlib.pyplot as plt
 # Read data
 from sklearn.svm import LinearSVC
-
+from sklearn.metrics import confusion_matrix
+from sklearn import metrics
+from sklearn.metrics import roc_curve, auc
+import dill
+from sklearn.externals import joblib
 
 def read_data():
     data = pd.read_csv("C:/Users/Apoorva/PycharmProjects/TweetsClassification/amazon-fine-foods/Reviews.csv", header=0, delimiter=",", quoting=15)
 
-    #14640 us airlines tweets
     print (data.shape)  # (25000, 3)
     #print (data["review"][0])  # Check out the review
     #print (data["sentiment"][0])  # Check out the sentiment (0/1)
@@ -68,11 +73,6 @@ def clean_text(text):
 
 
 
-from nltk import bigrams
-
-# print(list(bigrams("We are playing with bigrams".split())))
-
-# [('We', 'are'), ('are', 'playing'), ('playing', 'with'), ('with', 'bigrams')]
 
 def stemming_tokenizer(text):
     stemmer = PorterStemmer()
@@ -95,31 +95,186 @@ def get_tweets_feature_matrix(classifier, data):
 
     return feature_matrix
 
+def build_classifiers(train_X,train_y):
+        clf_logistic = Pipeline([
+            ('vectorizer', CountVectorizer(analyzer="word",
+                                           tokenizer=word_tokenize,
+                                           # ! Comment line to include mark_negation and uncomment next line
+                                           # tokenizer=lambda text: mark_negation(word_tokenize(text)),
+                                           preprocessor=lambda text: text.replace("<br />", " "),
+                                           max_features=10000)),
+            ('classifier', LogisticRegression(C=1e5))
+        ])
+        clf_multinomialNB = Pipeline([
+            ('vectorizer', CountVectorizer(analyzer="word",
+                                           tokenizer=word_tokenize,
+                                           # ! Comment line to include mark_negation and uncomment next line
+                                           # tokenizer=lambda text: mark_negation(word_tokenize(text)),
+                                           preprocessor=lambda text: text.replace("<br />", " "),
+                                           max_features=10000)),
+            ('classifier', MultinomialNB())
+        ])
 
-def unigrams(train_X,train_y,test_X,test_y):
+        clf_svm = Pipeline([
+            ('vectorizer', CountVectorizer(analyzer="word",
+                                           tokenizer=word_tokenize,
+                                           # ! Comment line to include mark_negation and uncomment next line
+                                           # tokenizer=lambda text: mark_negation(word_tokenize(text)),
+                                           preprocessor=lambda text: text.replace("<br />", " "),
+                                           max_features=10000)),
+            ('classifier', LinearSVC())
+        ])
+        clf_bernoulliNB = Pipeline([
+            ('vectorizer', CountVectorizer(analyzer="word",
+                                           tokenizer=word_tokenize,
+                                           # ! Comment line to include mark_negation and uncomment next line
+                                           # tokenizer=lambda text: mark_negation(word_tokenize(text)),
+                                           preprocessor=lambda text: text.replace("<br />", " "),
+                                           max_features=10000)),
+            ('classifier', BernoulliNB())
+        ])
+        clf = Pipeline([
+            ('vectorizer', TfidfVectorizer(tokenizer=stemming_tokenizer,
+                                            stop_words=stopwords.words('english') + list(string.punctuation))),
+             ('classifier', MultinomialNB(alpha=0.05)),
+         ])
+
+        model_logistic = clf_logistic.fit(train_X, train_y)
+        filename = 'model_logistic.sav'
+        with open(filename, 'wb') as f:
+             dill.dump(model_logistic,f)
+
+        model_multinomial = clf_multinomialNB.fit(train_X, train_y)
+        filename = 'model_multinomial.sav'
+        with open(filename, 'wb') as f:
+             dill.dump(model_multinomial,f)
+
+        # SVM MODEL
+        filename = 'model_svm.sav'
+
+        model_svm = clf_svm.fit(train_X, train_y)
+        with open(filename, 'wb') as f:
+            dill.dump(model_svm,f)
+
+        filename = 'model_bernoulli.sav'
+        model_bernoulli = clf_bernoulliNB.fit(train_X, train_y)
+        with open(filename, 'wb') as f:
+            dill.dump(model_bernoulli, f)
+
+def formatt(x):
+        if x == '0':
+            return 0
+        return 1
+
+def unigrams(test_X,test_y,data):
+
+    prediction = dict()
+    print("Loading..")
+    filename = 'model_multinomial.sav'
+
+    with open(filename, 'rb') as f:
+        clf2 = dill.load(f)
+    print("Loading..")
+
+    prediction['Multinomial'] = clf2.predict(test_X)
+    # prediction['Multinomial'] = model_multinomial.predict(test_X)
+    print("Predicting Multinomial NB..")
+    print(prediction['Multinomial'])
+    print("Printing Multinomial NB accuracy")       #0.843
+    result = clf2.score(test_X, test_y)
+    print(result)
+
+    # print(clf_multinomialNB.score(test_X, test_y))
 
 
-    clf = Pipeline([
-        ('vectorizer', CountVectorizer(analyzer="word",
-                                       tokenizer=word_tokenize,
-                                       # ! Comment line to include mark_negation and uncomment next line
-                                       # tokenizer=lambda text: mark_negation(word_tokenize(text)),
-                                       preprocessor=lambda text: text.replace("<br />", " "),
-                                       max_features=10000)),
-        ('classifier', MultinomialNB())
-    ])
-    # clf = Pipeline([
-    #     ('vectorizer', TfidfVectorizer(tokenizer=stemming_tokenizer,
-    #                                    stop_words=stopwords.words('english') + list(string.punctuation))),
-    #     ('classifier', MultinomialNB(alpha=0.05)),
-    # ])
+    # SVM MODEL
+    filename = 'model_svm.sav'
 
-    clf.fit(train_X, train_y)
-    print("Printing accuracy")
-    print(clf.score(test_X, test_y))
+    with open(filename, 'rb') as f:
+        clf2 = dill.load(f)
 
-    # with mark_negation 0.84760000000000002
-    # without mark_negation 0.84440000000000004
+    prediction['svm'] = clf2.predict(test_X)
+    print("Predicting SVM..")
+    print(prediction['svm'])
+    print("Printing SVM accuracy")                  #0.824
+    result = clf2.score(test_X, test_y)
+    print(result)
+
+    # print(clf_svm.score(test_X, test_y))
+
+
+    #BERNOULLI NB
+
+    filename = 'model_bernoulli.sav'
+    with open(filename, 'rb') as f:
+        clf2 = dill.load(f)
+
+    prediction['Bernoulli'] = clf2.predict(test_X)
+    print("Predicting Bernoulli NB..")
+    print(prediction['Bernoulli'])
+    print("Printing Bernoulli NB accuracy")                #0.77
+    # print(clf_bernoulliNB.score(test_X, test_y))
+
+    result = clf2.score(test_X, test_y)
+    print(result)
+
+    #Logistic Regression
+
+    filename = 'model_logistic.sav'
+    with open(filename, 'rb') as f:
+        clf2 = dill.load(f)
+
+    prediction['Logistic'] = clf2.predict(test_X)
+    print("Predicting Logistic Regression..")
+    print(prediction['Logistic'])
+    print("Printing Logistic Regression accuracy")
+    # print(clf_bernoulliNB.score(test_X, test_y))
+
+    result = clf2.score(test_X, test_y)             #0.821
+    print(result)
+
+    print("Confusion matrix for Multinomial ..")
+    print(metrics.classification_report(test_y, prediction['Multinomial'], target_names=["positive", "negative"]))
+
+    print("Confusion matrix for Logistic Regression ..")
+    print(metrics.classification_report(test_y, prediction['Logistic'], target_names=["positive", "negative"]))
+
+    print("Confusion matrix for SVM ..")
+    print(metrics.classification_report(test_y, prediction['svm'], target_names=["positive", "negative"]))
+
+    print("Confusion matrix for Bernoulli ..")
+    print(metrics.classification_report(test_y, prediction['Bernoulli'], target_names=["positive", "negative"]))
+
+    vfunc = np.vectorize(formatt)
+    cmp = 0
+    colors = ['b', 'g', 'y', 'm', 'k']
+    for model, predicted in prediction.items():
+        # print(vfunc(predicted))
+        # print(test_y)
+        # print(data["Sentiment"])
+        # print(np.array(data["Sentiment"]))
+        myarray = np.array(test_y)
+        # print(myarray)
+        newarray = []
+        for i in range(0, len(test_y)):
+            test_y[i] = int(test_y[i])
+            newarray.append(test_y[i])
+        # print(newarray)
+        false_positive_rate, true_positive_rate, thresholds = roc_curve(newarray, vfunc(predicted))
+        # roc_auc = auc(false_positive_rate, true_positive_rate)
+        # plt.plot(false_positive_rate, true_positive_rate, colors[cmp], label='%s: AUC %0.2f' % (model, roc_auc))
+        plt.plot(false_positive_rate, true_positive_rate, colors[cmp], label='%s:' % (model))
+
+        cmp += 1
+
+    plt.title('Classifiers comparaison with ROC')
+    plt.legend(loc='lower right')
+    plt.plot([0, 1], [0, 1], 'r--')
+    plt.xlim([-0.1, 1.2])
+    plt.ylim([-0.1, 1.2])
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.show()
 
     #feature_matrix = get_tweets_feature_matrix(clf, train_X)
     # print("Printing feature matrix")
@@ -184,24 +339,36 @@ def unigram_bigram(train_X,train_y,test_X,test_y):
 
 
 def main():
+    print("Reading Data..")
     data = read_data()
+    print("Data Fetched")
     # print(data["review"][0])
     sentiment_data = randomize(data)
-    train_X,train_y = train_data(sentiment_data)
+    print("Dividing into train data..")
+    train_X, train_y = train_data(sentiment_data)
+    print("Dividing into test data..")
     test_X,test_y = test_data(sentiment_data)
+    print("Cleaning data..")
     for i in range(0, len(train_X)):
-        train_X[i] = clean_text(train_X[i])
-    # print(train_X[0])
-    # print(train_y[0])
-
+        train_X[i] = clean_text(str(train_X[i]))
+    # # print(train_X[0])
+    # # print(train_y[0])
+    #
     for i in range(0, len(test_X)):
-        test_X[i] = clean_text(test_X[i])
+        test_X[i] = clean_text(str(test_X[i]))
     # print(test_X[0])
-    # print(test_y[0])
+    # print(test_y)
+    # exit(0)
     #print(random_data)
-    unigrams(train_X,train_y,test_X,test_y)     #0.843
-    bigrams(train_X,train_y,test_X,test_y)      #0.815
-    unigram_bigram(train_X,train_y,test_X,test_y)  #0.81
+
+    #Call this function to create and save models in file
+    # build_classifiers(train_X,train_y)
+
+    # Load saved models to test for accuracy
+    unigrams(test_X,test_y,data)
+
+    # bigrams(train_X,train_y,test_X,test_y)      #0.815
+    # unigram_bigram(train_X,train_y,test_X,test_y)  #0.81
 
 
 
